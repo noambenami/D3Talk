0f683795e1ccc46b5d34a775b45fa783194961ad
noambenami <noam.benami@mulesoft.com>
2015-04-01 22:19:29 -0700

    Added couple more samples

--- scripts/randomStream.js
+++ scripts/randomStream.js
@@ -1,18 +1,21 @@
 /**
  * @param size Array cardinality
- * @param range Maximum value of array elements
+ * @param maxValue Maximum value of array elements
  * @constructor
  */
-function RandomStream(size, range) {
-  var self = this;
-  var step = 2;
-  // Init the array with a simple seed
-  self.data = d3.range(0, size * step, step);
+function RandomStream(size, maxValue) {
+
+  maxValue = maxValue || 1;
+
+  var self    = this;
+
+  self.random = d3.random.normal(0, maxValue);
+  self.data   = d3.range(size).map(self.random);
 
   self.update = function () {
     self.data.shift();
-    self.data.push(Math.floor(Math.random() * range));
+    self.data.push(self.random());
     return self.data;
-  }
+  };
 
 }
