4f7af9074e8b22336970949257d2a342b55de832
noambenami <noam.benami@mulesoft.com>
2015-04-02 17:17:51 -0700

    Fixed issues, added fun text interpolation

--- scripts/simpleStream.js
+++ scripts/simpleStream.js
@@ -3,17 +3,16 @@
  * @param maxValue Maximum value of array elements
  * @constructor
  */
-function RandomStream(size, maxValue) {
+function SimpleStream(size, maxValue) {
 
   maxValue = maxValue || 1;
 
   var self    = this;
-  self.random = d3.random.normal(0, maxValue);
-  self.data   = d3.range(size).map(self.random);
+  self.data   = d3.range(size);
 
   self.update = function () {
     self.data.shift();
-    self.data.push(self.random());
+    self.data.push(Math.floor(Math.random() * maxValue));
     return self.data;
   };
 
